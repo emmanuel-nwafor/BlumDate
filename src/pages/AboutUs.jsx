39ab14e1cde62f063a38acf9c457c37b8301128d
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-06-18 18:10:35 -0700

    Fixed about us overlay

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Header from '../components/Header'
+import React from 'react';
+import Header from '../components/Header';
 
 export default function AboutUs() {
   return (
     <div>
-      <div className="justify-center items-center flex h-72 sm:h-96 bg-[url(https://res.cloudinary.com/dbczfoqnc/image/upload/v1749845337/4b28601c830887a7f8ec2ba8ad92a5f3586b8c05_1_gzs0lt.jpg)] bg-cover bg-center hero-overlay">
-       <Header />
-       <h1 className="text-3xl text-white font-bold text-center">
-        About us
-       </h1>
+      <div className="relative h-72 sm:h-96 bg-cover bg-center bg-[url(https://res.cloudinary.com/dbczfoqnc/image/upload/v1749845337/4b28601c830887a7f8ec2ba8ad92a5f3586b8c05_1_gzs0lt.jpg)]">
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-black bg-opacity-50 z-0" />
+
+        {/* Content */}
+        <div className="relative z-10 flex flex-col justify-center items-center h-full">
+          <Header />
+          <h1 className="text-3xl text-white font-bold mt-6">About us</h1>
+        </div>
       </div>
 
-      <h1 className="text-4xl">
-        Hello
-      </h1>
+      <h1 className="text-4xl">Hello</h1>
     </div>
-  )
+  );
 }
